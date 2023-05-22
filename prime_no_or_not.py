49cbe4187cc72dfcfabb39808b1dcdb6897347a0
pragya <129305135+pragya-sirse@users.noreply.github.com>
2023-05-22 09:53:15 +0530

    Update prime_no_or_not.py

--- prime_no_or_not.py
+++ prime_no_or_not.py
@@ -1,9 +1,14 @@
 
 n=90
-if n%2==1:
-    print("prime no")
-    'break'
+if n>1:
+    for i in range (2,int(n/2)+1):
+        if (n%i)==0:
+          print("not an prime no")
+         'break'
+     else:
+      print(" prime no")
 else:
-    print("not an prime no")
+     print(" prime no")
+    
 
 
